25cbf7d80bc163034f6e878cd4f0556dcf13e4a1
Vadym Stebakov <vadim.stebakov.94@gmail.com>
2023-08-25 19:44:28 +0300

    build: adjust github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://vadymstebakov.github.io',
+    base: '/me',
     server: { port: 3000, host: true },
 });
